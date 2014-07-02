e63ef70605bea03c471529056a3d9733216f2681
David Golden <dagolden@cpan.org>
2014-07-01 23:17:05 -0400

    add more documentation

--- lib/Hash/Ordered.pm
+++ lib/Hash/Ordered.pm
@@ -14,6 +14,15 @@ use constant {
     _KEYS => 1,
 };
 
+=method new
+
+    $oh = Hash::Ordered->new;
+    $oh = Hash::Ordered->new( @pairs );
+
+Constructs an object, with an optional list of key-value pairs.
+
+=cut
+
 sub new {
     my ( $class, @pairs ) = @_;
     Carp::croak("new() requires key-value pairs") unless @pairs % 2 == 0;
@@ -23,31 +32,80 @@ sub new {
     return bless $self, $class;
 }
 
+=method clone
+
+    $oh2 = $oh->clone;
+
+Creates a shallow copy of another object.
+
+=cut
+
 sub clone {
     my ($self) = @_;
     my $clone = [ { %{ $self->[_DATA] } }, [ @{ $self->[_KEYS] } ] ];
     return bless $clone, ref $self;
 }
 
+=method keys
+
+    @keys = $oh->keys;
+
+Returns the ordered list of keys.
+
+=cut
+
 sub keys {
     my ($self) = @_;
     return @{ $self->[_KEYS] };
 }
+
+=method values
+
+    @values = $oh->values;
+
+Returns the ordered list of values.
+
+=cut
 
 sub values {
     my ($self) = @_;
     return map { $self->[_DATA]{$_} } @{ $self->[_KEYS] };
 }
 
+=method exists
+
+    if ( $oh->exists("some key") ) { ... }
+
+Test if some key exists in the hash (without creating it).
+
+=cut
+
 sub exists {
     my ( $self, $key ) = @_;
     return exists $self->[_DATA]{$key};
 }
 
+=method get
+
+    $value = $oh->get("some key");
+
+Returns the value associated with the key.
+
+=cut
+
 sub get {
     my ( $self, $key ) = @_;
     return $self->[_DATA]{$key};
 }
+
+=method set
+
+    $oh->set("some key" => "some value");
+
+Associates a value with a key and returns the value.  If the key does not
+already exist in the hash, it will be added at the end.
+
+=cut
 
 sub set {
     my ( $self, $key, $value ) = @_;
@@ -56,6 +114,15 @@ sub set {
     }
     return $self->[_DATA]{$key} = $value;
 }
+
+=method delete
+
+    $value = $oh->delete("some key");
+
+Removes a key-value pair from the hash and returns the value.  This
+is expensive, as the ordered list of keys has to be updated.
+
+=cut
 
 sub delete {
     my ( $self, $key ) = @_;
@@ -68,6 +135,18 @@ sub delete {
     }
     return undef;
 }
+
+=method push
+
+    $oh->push( one => 1, two => 2);
+
+Add a list of key-value pairs to the end of the ordered hash.  If a key already
+exists in the hash, it will be deleted and re-inserted at the end with the new
+value.
+
+Returns the number of keys after the push is complete.
+
+=cut
 
 sub push {
     my ( $self, @pairs ) = @_;
@@ -86,11 +165,29 @@ sub push {
     return scalar $self->keys;
 }
 
+=method pop
+
+    ($key, $value) = $oh->pop;
+
+Removes and returns the last key-value pair in the ordered hash.
+
+=cut
+
 sub pop {
     my ($self) = @_;
     my $key = pop @{ $self->[_KEYS] };
     return $key, delete $self->[_DATA]{$key};
 }
+
+=method unshift
+
+    $oh->unshift( one => 1, two => 2 );
+
+Adds a list of key-value pairs to the beginning of the ordered hash.  If a key
+already exists, it will be deleted and re-inserted at the beginning with the
+new value.
+
+=cut
 
 sub unshift {
     my ( $self, @pairs ) = @_;
@@ -109,16 +206,47 @@ sub unshift {
     return scalar $self->keys;
 }
 
+=method shift
+
+    ($key, $value) = $oh->shift;
+
+Removes and returns the first key-value pair in the ordered hash.
+
+=cut
+
 sub shift {
     my ($self) = @_;
     my $key = shift @{ $self->[_KEYS] };
     return $key, delete $self->[_DATA]{$key};
 }
 
+=method as_list
+
+    @pairs = $oh->as_list;
+
+Returns the ordered list of key-value pairs.
+
+=cut
+
 sub as_list {
     my ($self) = @_;
     return map { ; $_ => $self->[_DATA]{$_} } @{ $self->[_KEYS] };
 }
+
+=method iterator
+
+    $iter = $oh->iterator
+
+    while ( my ($key,$value) = $iter->() ) { ... }
+
+Returns a code reference that returns a single key-value pair (in order) on
+each invocation, or the empty list if all keys are visited.
+
+The list of keys to return is set when the iterator is generator.  Keys
+added later will not be returned and keys deleted will be returned with
+C<undef>.
+
+=cut
 
 sub iterator {
     my ($self) = @_;
@@ -132,19 +260,17 @@ sub iterator {
 
 1;
 
-=for Pod::Coverage BUILD
-
 =head1 SYNOPSIS
 
     use Hash::Ordered;
 
     my $oh = Hash::Ordered->new( a => 1 );
 
-    $oh->get( 'a');
+    $oh->get( 'a' );
     $oh->set( 'a' => 2 );
 
-    $oh->exists('a');
-    $oh->delete('a');
+    $oh->exists( 'a' );
+    $val = $oh->delete( 'a' );
 
     @keys  = $oh->keys;
     @vals  = $oh->values;
@@ -153,25 +279,90 @@ sub iterator {
     $oh->push( c => 3, d => 4 );
     $oh->unshift( e => 5, f => 6 );
 
-    ($k,$v) = $oh->pop;
-    ($k,$v) = $oh->shift;
+    ( $k, $v ) = $oh->pop;
+    ( $k, $v ) = $oh->shift;
 
     $iter = $oh->iterator;
-    while( ($k, $v) = $iter->() ) { ... }
+    while( ( $k, $v ) = $iter->() ) { ... }
 
 =head1 DESCRIPTION
 
-This module might be cool, but you'd never know it from the lack
-of documentation.
+This module implements an ordered hash, meaning that it associates keys with
+values like a Perl hash, but keeps the keys in a consistent order.  Because it
+is implemented as an object and manipulated with method calls, it is much
+slower than a Perl hash.  This is the cost of keeping order.
 
-=head1 USAGE
+=head1 MOTIVATION
 
-Good luck!
+For a long time, I used L<Tie::IxHash> for ordered hashes, but I grew
+frustrated with things it lacked, like a cheap way to copy an IxHash object or
+a convenient iterator when not using the tied interface.  As I looked at its
+implementation, it seemed more complex than I though it needed, with an extra
+level of indirection that slows data access.
+
+Given that frustration, I started experimenting with the simplest thing I
+thought could work for an ordered hash.
+
+As I worked on this, I also started searching for other modules doing similar
+things.  What I found fell broadly into two camps: modules based on tie (even
+if they offered an OO interface), and pure OO modules.  They all either lacked
+features I deemed necessary or else seemed overly complex in either
+implementation or API.
+
+Hash::Ordered attempts to find the sweet spot with simple implementation,
+reasonably good efficiency for most common operations, and a rich, intuitive
+API.
 
 =head1 SEE ALSO
 
+=head2 Tie modules
+
+The following modules offer some sort of tie interface.  I don't like ties, in
+general, because of the extra indirection involved over a direct method call, but
+if that doesn't bother you, you might want to try one of these
+
+L<Tie::IxHash> is probably the most well known and includes an OO API.  If its
+warts and performance profile aren't a problem, it might serve.
+
+L<Tie::LLHash> I haven't used, but the linked-list implementation might be
+worthwhile if you expect to do a lot of deletions.
+
+L<Tie::Hash::Indexed> is implemented in XS and thus seems promising if pure-Perl
+isn't a criterion, but last I looked, it had a lot of CPAN Testers failures,
+which lowers my trust in it.
+
+These other modules have very specific designs/limitations and I didn't find
+any of them suitable for general purpose use:
+
 =for :list
-* Maybe other modules do related things.
+* L<Tie::Array::AsHash> — array elements split with separator; tie API only
+* L<Tie::Hash::Array> — ordered alphabetically; tie API only
+* L<Tie::InsertOrderHash> — ordered by insertion; tie API only
+* L<Tie::StoredOrderHash> — ordered by last update; tie API only
+
+=head2 Other ordered hash modules
+
+Other modules stick with an object-oriented API, with a wide variety of
+implementation approaches.
+
+L<Array::AsHash> is essentially an inverse implementation from Hash::Ordered.
+It keeps pairs in an array and uses a hash to index into the array.  I think
+this indirection makes hash-like operations slower, but getting the list of
+pairs back out is much faster.  It takes an arrayref to initialize, but can
+shallow copy it if needed.  I think this is a reasonable alternative if static
+construction and listing out contents is more common than individual item
+access.
+
+These other modules have restrictions or particularly complicated
+implementations (often relying on C<tie>) and thus I didn't think any of them
+suitable for general purpose use:
+
+=for :list
+* L<Array::Assign> — arrays with named access; restricted keys
+* L<Array::OrdHash> — overloads array/hash deref and uses internal tied data
+* L<Data::Pairs> — array of key-value hashrefs; allows duplicate keys
+* L<Data::OMap> — array of key-value hashrefs; no duplicate keys
+* L<Data::XHash> — blessed, tied hashref with doubly-linked-list
 
 =cut
 
