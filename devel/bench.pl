57893d136195b7ac1cb6ee46d46d36b747763b71
David Golden <dagolden@cpan.org>
2014-07-02 18:35:29 -0400

    add Tie::LLHash to benchmarking

--- devel/bench.pl
+++ devel/bench.pl
@@ -5,6 +5,7 @@ use warnings;
 
 use lib 'lib';
 use Tie::IxHash;
+use Tie::LLHash;
 use Hash::Ordered;
 
 use Benchmark qw( cmpthese );
@@ -14,6 +15,7 @@ cmpthese(
     $count,
     {
         'Tie::IxHash'   => \&ix,
+        'Tie::LLHash'   => \&ll,
         'Hash::Ordered' => \&oh,
     }
 );
@@ -24,7 +26,7 @@ sub ix {
     my $h = Tie::IxHash->new;
     $h->STORE( rand(), rand() ) for 1 .. SIZE;
     for my $k ( $h->Keys ) {
-        $h->FETCH($k) if $h->EXISTS($k);
+        my $val = $h->FETCH($k) if $h->EXISTS($k);
     }
     my @list = map { $_ => $h->FETCH($_) } $h->Keys;
     $h->DELETE($_) for $h->Keys;
@@ -34,8 +36,19 @@ sub oh {
     my $h = Hash::Ordered->new;
     $h->set( rand(), rand() ) for 1 .. SIZE;
     for my $k ( $h->keys ) {
-        $h->get($k) if $h->exists($k);
+        my $val = $h->get($k) if $h->exists($k);
     }
     my $list = $h->as_list;
     $h->delete($_) for $h->keys;
+}
+
+sub ll {
+    tie my %hash, 'Tie::LLHash';
+    my $h = tied(%hash);
+    $h->last( rand(), rand() ) for 1 .. SIZE;
+    for my $k ( keys %hash ) {
+        my $val = $hash{$k} if exists $hash{$k};
+    }
+    my @list = map { $_ => $hash{$_} } keys %hash;
+    delete $hash{$_} for keys %hash;
 }
